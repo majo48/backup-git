5e6b3bed0aebcdf4ee987ab662b9de0e49c5ce21
majo48 <martin.jonasse@mail.ch>
2025-01-02 13:18:45 +0100

    Successfully mounted NAS using automation.

--- backup/backup2nas.py
+++ backup/backup2nas.py
@@ -1,15 +1,26 @@
 #!/bin/sh
 """
 Backup a list of folders to a NAS, using macOS CLI modules and Python.
+Connecting to the NAS using the terminal or scripts is not without problems.
+
+Prerequisites:
+=============
+    1. Use Finder Go > Connect to server > servername > connect > username + password 
+    2. Select folder myMacMini
+    3. Open Finder-Settings > General > set Connected servers > creates myMacMini icon
+    4. Open System-Setting > Login Items & Extensions > Open at Login > drag myMacMini icon to list
+    5. Restart computer
+    6. Location /Volumes/myMacMini is permanent
 """
 
-from decouple import config
 import sys
 from sys import platform
 import os
 import subprocess
 from subprocess import check_output
 
+# ====
+# current user
 print("Username: "+os.getlogin())
 
 # ====
@@ -17,6 +28,7 @@ print("Username: "+os.getlogin())
 if platform != "darwin":
     print("*** Error: script not compatible with the current operating system")
     sys.exit(1)  
+print("Operating System: mac OS")
 
 # ====
 # check that Finder app is running
@@ -25,43 +37,17 @@ pid = sp.stdout
 if pid == '':
     print("*** Error: the Finder application must be active/running.")
     sys.exit(2)
+print("Finder app: active")
 
 # ====
-# Mount NAS: //username:password@servername/rootdirectory
-# get NAS parameters
-servername = config('NASSERVERNAME')       # the network share name of the NAS
-rootdirectory = config('NASROOTDIRECTORY') # the root directory from above example
-# get credentials
-username = config('NASUSERNAME') # user name in NAS
-password = config('NASPASSWORD') # user password in NAS
-# set mount parameters
-fqn = "//"+username+":"+password+"@"+servername+"/"+rootdirectory
-mountpoint = "/Volumes/smb"
-# mount action
-result = subprocess.run( ["mount", "-t", "smbfs", fqn, mountpoint], capture_output=True, text=True )
-if result.returncode != 0:
-    print("*** Error: mount operation was unsuccessfull, code: "+str(result.returncode))
-    print("stdout: "+result.stdout)
-    print("stderr: "+result.stderr)
-    sys.exit(3)
-    """
-    mount: /Volumes/smb: invalid file system.
-    mount_smbfs: mount error: /Volumes/smb: Operation not permitted
-        mount: /Volumes/smb failed with 64
-    mount_smbfs: mount error: /Volumes/smb: Operation not permitted
-        mount: /Volumes/smb failed with 64
-    mount_smbfs: server connection failed: Operation timed out
-        mount: /Volumes/smb failed with 68
-    """
-
-print("Successfully mounted NAS.")
-""" 
-    work in progress, see: 
-        https://apple.stackexchange.com/questions/697/how-can-i-mount-an-smb-share-from-the-command-line
-        https://discussions.apple.com/thread/4927134?sortBy=rank
-"""
+# check that NAS is mounted
+location = "/Volumes/myMacMini"
+file="_readme.txt"
+if os.path.isfile(location+"/"+file):
+    print('Mount point: exists')
+else:
+    print('Error: cannot find /Volumes/myMacMini/_readme.txt')
 
 # ====
 # Close script
-result = subprocess.run( ["umount", mountpoint] )
 sys.exit(0) # finished
